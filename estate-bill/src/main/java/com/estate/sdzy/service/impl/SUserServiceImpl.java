6063c6523dc76abd8d69ac60c7e2e380b27cbdb3
mamaoqing <luyuna1121@163.com>
2020-08-04 15:42:08 +0800

    修改日志打印

--- estate-bill/src/main/java/com/estate/sdzy/service/impl/SUserServiceImpl.java
+++ estate-bill/src/main/java/com/estate/sdzy/service/impl/SUserServiceImpl.java
@@ -73,7 +73,7 @@ public class SUserServiceImpl extends ServiceImpl<SUserMapper, SUser> implements
 
         int insert = userMapper.insert(user);
         if (insert > 0) {
-            log.info("自动添加公司管理员角色，登录用户名={}admin,密码=123456", userName);
+            log.info("自动添加公司管理员角色，登录用户名={},密码=123456", userName);
         } else {
             log.error("自动添加管理员角色失败,请联系管理员添加账号");
             throw new BillException(BillExceptionEnum.SYSTEM_INSERT_ERROR);
