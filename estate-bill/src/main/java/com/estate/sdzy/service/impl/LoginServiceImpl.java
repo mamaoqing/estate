bc2f5480e507e66ca74dcbb59233767fc85009e2
mamaoqing <luyuna1121@163.com>
2020-08-11 19:03:04 +0800

    优化菜单查询

--- estate-bill/src/main/java/com/estate/sdzy/service/impl/LoginServiceImpl.java
+++ estate-bill/src/main/java/com/estate/sdzy/service/impl/LoginServiceImpl.java
@@ -42,8 +42,8 @@ public class LoginServiceImpl implements LoginService {
             String key = String.valueOf(UUID.randomUUID());
             String token = key+":"+user.getUserName();
             // 设置过期时间单位秒
-            redisUtil.set(token, user, 50 * 60);
-            return ResultUtil.success(token);
+            redisUtil.set(token, user, 500 * 60);
+            return ResultUtil.success(token+";"+user.getCompId());
         }else{
             return ResultUtil.error("登陆失败，请联系管理员",0);
         }
