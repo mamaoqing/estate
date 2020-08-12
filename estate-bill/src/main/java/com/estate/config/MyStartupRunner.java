9a7ac7efd8828b3171f4dccdd1bb1fd64f3aa8dc
chenfangyuan <2237304258@qq.com>
2020-08-12 10:43:48 +0800

    自定义启动类

---
+++ estate-bill/src/main/java/com/estate/config/MyStartupRunner.java
@@ -0,0 +1,32 @@
+package com.estate.config;
+
+import com.estate.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @program: estate-parent
+ * @description: 项目启动时执行类
+ * @author: cfy
+ * @create: 2020-08-12 09:51
+ **/
+
+@Component
+public class MyStartupRunner {
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @PostConstruct
+     public void test() {
+        if (StringUtils.isEmpty(redisUtil.get("District_Number"))){
+
+        }else {
+            System.out.println("=============================================");
+            System.out.println("");
+            System.out.println("=============================================");
+        }
+    }
+}
