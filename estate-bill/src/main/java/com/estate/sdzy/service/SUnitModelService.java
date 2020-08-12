6ff2c64d62ccb6340fd673282f1e40bf8f6526fc
mzhongcai <javamzc@126.com>
2020-08-12 17:24:02 +0800

    修改单元型号列表返回值类型为list

--- estate-bill/src/main/java/com/estate/sdzy/service/SUnitModelService.java
+++ estate-bill/src/main/java/com/estate/sdzy/service/SUnitModelService.java
@@ -1,10 +1,10 @@
 package com.estate.sdzy.service;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.estate.exception.BillException;
 import com.estate.sdzy.entity.SUnitModel;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,7 +13,7 @@ import java.util.Map;
  * @since 2020-08-11
  */
 public interface SUnitModelService extends IService<SUnitModel> {
-    Page<SUnitModel> listUnitModel(Map<String,String> map, Integer pageNo, Integer size, String token);
+    List<SUnitModel> listUnitModel(Map<String,String> map, Integer pageNo, Integer size, String token);
     boolean saveOrUpdate(SUnitModel sUnitModel, String token) throws BillException;
     boolean save(SUnitModel sUnitModel,String token) throws BillException;
     boolean remove(Long id,String token) throws BillException;
