7b65813791a05f7b3a8a100403a22966da678b06
mzhongcai <javamzc@126.com>
2020-08-14 19:15:33 +0800

    增加日期格式化

--- estate-bill/src/main/java/com/estate/sdzy/entity/SDict.java
+++ estate-bill/src/main/java/com/estate/sdzy/entity/SDict.java
@@ -51,7 +51,7 @@ public class SDict implements Serializable {
     private String remark;
 
     @TableField(fill = FieldFill.INSERT)
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createdAt;
 
     private Long createdBy;
