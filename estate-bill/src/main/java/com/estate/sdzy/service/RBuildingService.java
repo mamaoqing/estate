4ce66086b840d9f3141d2c45dc51958d50875282
mzhongcai <javamzc@126.com>
2020-08-14 19:14:52 +0800

    增加建筑的后台方法

--- estate-bill/src/main/java/com/estate/sdzy/service/RBuildingService.java
+++ estate-bill/src/main/java/com/estate/sdzy/service/RBuildingService.java
@@ -1,7 +1,11 @@
 package com.estate.sdzy.service;
 
-import com.estate.sdzy.entity.RBuilding;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.estate.sdzy.entity.RBuilding;
+import com.estate.sdzy.entity.RCommunity;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -13,4 +17,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface RBuildingService extends IService<RBuilding> {
 
+    boolean save(RBuilding rBuilding,String token);
+    boolean update(RBuilding rBuilding,String token);
+    boolean delete(Long id,String token);
+    List<RBuilding> list(Map<String,String> map,Integer pageNo, Integer size);
+    Integer listNum(Map<String,String> map);
+    List<RCommunity> getUserComm(String token, Long compId);
+    String checkBuildingRoomUnit(Long compId);
 }
