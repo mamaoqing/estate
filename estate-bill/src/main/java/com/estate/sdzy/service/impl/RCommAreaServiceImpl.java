f9b4ee0ce85f9c249e8a0083893bfaf05809e1c5
mzhongcai <javamzc@126.com>
2020-08-14 09:42:12 +0800

    根据社区id获取分区

--- estate-bill/src/main/java/com/estate/sdzy/service/impl/RCommAreaServiceImpl.java
+++ estate-bill/src/main/java/com/estate/sdzy/service/impl/RCommAreaServiceImpl.java
@@ -1,10 +1,13 @@
 package com.estate.sdzy.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.estate.sdzy.entity.RCommArea;
 import com.estate.sdzy.mapper.RCommAreaMapper;
 import com.estate.sdzy.service.RCommAreaService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class RCommAreaServiceImpl extends ServiceImpl<RCommAreaMapper, RCommArea> implements RCommAreaService {
 
+    private RCommAreaMapper rCommAreaMapper;
+    @Override
+    public List<RCommArea> getCommArea(Long commId) {
+        QueryWrapper<RCommArea> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("comm_id",commId);
+        List<RCommArea> rCommAreas = rCommAreaMapper.selectList(queryWrapper);
+        return rCommAreas;
+    }
 }
