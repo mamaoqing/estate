c4621d9a46a9f761c77f1d418b56f133b3146283
mzhongcai <javamzc@126.com>
2020-08-20 18:05:51 +0800

    获取建筑和单元的id

--- estate-bill/src/main/java/com/estate/sdzy/asstes/mapper/RParkingSpaceMapper.java
+++ estate-bill/src/main/java/com/estate/sdzy/asstes/mapper/RParkingSpaceMapper.java
@@ -1,13 +1,11 @@
 package com.estate.sdzy.asstes.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.estate.sdzy.asstes.entity.RParkingSpace;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * <p>
@@ -28,4 +26,8 @@ public interface RParkingSpaceMapper extends BaseMapper<RParkingSpace> {
     Long getCompIdByName(@Param("name") String compName);
 
     Long getAreaIdByName(@Param("name") String areaName);
+
+    Long getBuildingIdByName(@Param("name") String buildingName);
+
+    Long getUnitIdByName(@Param("name") String unitName);
 }
