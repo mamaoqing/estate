f4da9117f9dc1bc82d4a91f6f77198769b2e1e30
mzhongcai <javamzc@126.com>
2020-08-24 14:22:49 +0800

    去掉字典名称属性

--- estate-bill/src/main/java/com/estate/sdzy/asstes/mapper/RRoomMapper.java
+++ estate-bill/src/main/java/com/estate/sdzy/asstes/mapper/RRoomMapper.java
@@ -22,6 +22,10 @@ public interface RRoomMapper extends BaseMapper<RRoom> {
                             @Param("buildingName")String buildingName, @Param("unitName")String unitName, @Param("roomNo")String roomNo,
                             @Param("roomModel")String roomModel, @Param("usable")String usable,
                             @Param("pageNo") Integer pageNo, @Param("size") Integer size, @Param("userId") Long userId);
+    Integer getListRoomNum(@Param("compName")String compName, @Param("commName")String commName, @Param("commAreaName")String commAreaName,
+                           @Param("buildingName")String buildingName, @Param("unitName")String unitName, @Param("roomNo")String roomNo,
+                           @Param("roomModel")String roomModel, @Param("usable")String usable,
+                           @Param("pageNo") Integer pageNo, @Param("size") Integer size, @Param("userId") Long userId);
     int updateBatch(@Param("userId")Long userId,@Param("userName")String userName,@Param("ids")String[] ids);
     int updateOwnerProperty(@Param("userId")Long userId,@Param("userName")String userName,@Param("ids")String[] ids);
 }
