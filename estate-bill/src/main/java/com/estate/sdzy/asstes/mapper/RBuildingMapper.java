e4bf8143dde2af3bf4d4199639787f83f5f300c3
mzhongcai <javamzc@126.com>
2020-08-24 14:21:46 +0800

    去掉字典名称属性

--- estate-bill/src/main/java/com/estate/sdzy/asstes/mapper/RBuildingMapper.java
+++ estate-bill/src/main/java/com/estate/sdzy/asstes/mapper/RBuildingMapper.java
@@ -24,10 +24,15 @@ public interface RBuildingMapper extends BaseMapper<RBuilding> {
     @Sql("update r_building set is_delete='1',modified_by=#{id},modified_name=#{name} where id=#{id}")
     int update(@Param("id") Long id,@Param("name") String name);
 
-    List<RBuilding> getListBuilding(@Param("name") String name,@Param("no") String no,@Param("type") String type, @Param("compId") String compId,@Param("commId") String  commId,@Param("commAreaId") String commAreaId ,@Param("pageNo") Integer pageNo,@Param("size") Integer size,@Param("userId") Long userId);
-    Integer insertUnitCopy(@Param("userId")Long userId, @Param("userName")String userName, @Param("rUnits")List<RUnit> rUnits, @Param("buildingId")Long buildingId);
-
-    Integer insertRoomCopy(@Param("userId")Long userId,@Param("userName")String userName,@Param("newUnitId")Integer newUnitId,@Param("unitId")Long unitId,@Param("buildingId")Long buildingId);
+    List<RBuilding> getListBuilding(@Param("name") String name,@Param("no") String no,@Param("type") String type,
+                                    @Param("compId") String compId,@Param("commId") String  commId,@Param("commAreaId") String commAreaId ,
+                                    @Param("pageNo") Integer pageNo,@Param("size") Integer size,@Param("userId") Long userId);
+    Integer getListBuildingNum(@Param("name") String name,@Param("no") String no,@Param("type") String type,
+                       @Param("compId") String compId,@Param("commId") String  commId,@Param("commAreaId") String commAreaId ,
+                       @Param("pageNo") Integer pageNo,@Param("size") Integer size,@Param("userId") Long userId);
+    void insertUnitCopy(@Param("userId")Long userId, @Param("userName")String userName, @Param("rUnits")List<RUnit> rUnits, @Param("buildingId")Long buildingId);
+    void insertUnitCopy1(List<RUnit> rUnits);
+    Integer insertRoomCopy(@Param("userId")Long userId,@Param("userName")String userName,@Param("rUnits")List<RUnit> rUnits,@Param("buildingId")Long buildingId);
     Integer checkName(@Param("name")String name,@Param("commAreaId")Long commAreaId);
     Integer checkNo(@Param("no")String no,@Param("commAreaId")Long commAreaId);
 }
