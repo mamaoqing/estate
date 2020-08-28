2d2caedac8c31caab6db5d00914a2f651d4d4224
mzhongcai <javamzc@126.com>
2020-08-28 15:43:10 +0800

    添加属性

--- estate-bill/src/main/java/com/estate/sdzy/tariff/entity/FMeterRecord.java
+++ estate-bill/src/main/java/com/estate/sdzy/tariff/entity/FMeterRecord.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.estate.sdzy.common.annotation.ExcelAnnotation;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -31,8 +33,12 @@ public class FMeterRecord implements Serializable {
     private Long id;
 
     private Long compId;
+    @TableField(exist = false)
+    private String compName;
 
     private Long commId;
+    @TableField(exist = false)
+    private String commName;
 
     private Long meterId;
 
@@ -40,6 +46,11 @@ public class FMeterRecord implements Serializable {
      * 物业类型：房产、停车位
      */
     private String propertyType;
+    /**
+     * 物业编号
+     */
+    @TableField(exist = false)
+    private String propertyName;
 
     /**
      * 物业id：房产id、停车位id
@@ -50,6 +61,15 @@ public class FMeterRecord implements Serializable {
      * 仪表类型（水表、电表、煤气表）
      */
     private String type;
+    /**
+     * 仪表编号
+     */
+    @TableField(exist = false)
+    private String no;
+    /**
+     * 仪表名称
+     */
+    private String name;
 
     /**
      * 操作类型（抄表、出账单、缴费）
@@ -73,5 +93,15 @@ public class FMeterRecord implements Serializable {
 
     private String createdName;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @ExcelAnnotation(value = "修改时间")
+    private Date modifiedAt;
+
+    private Long modifiedBy;
+    @ExcelAnnotation(value = "修改人")
+    private String modifiedName;
+
+    private Integer isDelete;
 
 }
