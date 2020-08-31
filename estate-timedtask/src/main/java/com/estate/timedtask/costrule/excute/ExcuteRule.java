eb0de312b2a92adb2ab42cd263b52b80739eac68
mamaoqing <luyuna1121@163.com>
2020-08-31 16:13:15 +0800

    定时任务

--- estate-timedtask/src/main/java/com/estate/timedtask/costrule/excute/ExcuteRule.java
+++ estate-timedtask/src/main/java/com/estate/timedtask/costrule/excute/ExcuteRule.java
@@ -12,31 +12,39 @@ import java.util.Map;
 public class ExcuteRule {
 
     /**
-     *  获取到房产，跟停车位的id列表
+     * 获取到房产，跟停车位的id列表
+     *
      * @param id 费用标准id
      * @return 返回一个map
-     * @throws SQLException sql
+     * @throws SQLException           sql
      * @throws ClassNotFoundException 找不到类。mariadb
      */
-    public static Map<String,List<Integer>> month(Integer id) throws SQLException, ClassNotFoundException {
-        String sql = "select property_type,property_id from f_cost_rule_room where 1=1 and cost_rule_id = "+id;
-        ResultSet resultSet = ConnectUtil.executeQuery(sql);
+    public static Map<String, List<Integer>> month(Integer id) throws ClassNotFoundException {
+        Map<String, List<Integer>> map = new HashMap<String, List<Integer>>(16);
+        String sql = "select property_type,property_id from f_cost_rule_room where 1=1 and cost_rule_id = " + id;
+        ResultSet resultSet = null;
         List<Integer> roomList = new ArrayList<Integer>();
         List<Integer> parkList = new ArrayList<Integer>();
-        Map<String,List<Integer>> map = new HashMap<String, List<Integer>>(16);
-        while (resultSet.next()){
-            String property_type = resultSet.getString("property_type");
-            int property_id = resultSet.getInt("property_id");
-            if("room".equals(property_type)){
-                roomList.add(property_id);
+        try {
+            resultSet = ConnectUtil.executeQuery(sql);
+            while (resultSet.next()) {
+                String property_type = resultSet.getString("property_type");
+                int property_id = resultSet.getInt("property_id");
+                if ("room".equals(property_type)) {
+                    roomList.add(property_id);
+                }
+                if ("park".equals(property_type)) {
+                    parkList.add(property_id);
+                }
             }
-            if("park".equals(property_type)){
-                parkList.add(property_id);
-            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }finally {
         }
 
-        map.put("room",roomList);
-        map.put("park",parkList);
+
+        map.put("room", roomList);
+        map.put("park", parkList);
         return map;
     }
 }
