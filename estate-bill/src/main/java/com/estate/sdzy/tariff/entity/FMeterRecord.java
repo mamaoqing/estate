6c101231200b0d396d310ff4723ee82cb22ea5d3
mzhongcai <javamzc@126.com>
2020-09-01 11:18:26 +0800

    增加导出注解

--- estate-bill/src/main/java/com/estate/sdzy/tariff/entity/FMeterRecord.java
+++ estate-bill/src/main/java/com/estate/sdzy/tariff/entity/FMeterRecord.java
@@ -35,10 +35,12 @@ public class FMeterRecord implements Serializable {
 
     private Long compId;
     @TableField(exist = false)
+    @ExcelAnnotation(value = "物业公司",master = true)
     private String compName;
 
     private Long commId;
     @TableField(exist = false)
+    @ExcelAnnotation(value = "社区",master = true)
     private String commName;
 
     private Long meterId;
@@ -46,11 +48,13 @@ public class FMeterRecord implements Serializable {
     /**
      * 物业类型：房产、停车位
      */
+    @ExcelAnnotation(value = "物业类型")
     private String propertyType;
     /**
      * 物业编号
      */
     @TableField(exist = false)
+    @ExcelAnnotation(value = "物业编号")
     private String propertyName;
 
     /**
@@ -61,15 +65,19 @@ public class FMeterRecord implements Serializable {
     /**
      * 仪表类型（水表、电表、煤气表）
      */
+    @ExcelAnnotation(value = "仪表类型",master = true)
     private String type;
     /**
      * 仪表编号
      */
     @TableField(exist = false)
+    @ExcelAnnotation(value = "仪表编号",master = true)
     private String no;
     /**
      * 仪表名称
      */
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "仪表名称")
     private String name;
 
     /**
@@ -80,23 +88,30 @@ public class FMeterRecord implements Serializable {
     /**
      * 当前数量
      */
+    @ExcelAnnotation(value = "抄表刻度",master = true)
     private BigDecimal newNum;
 
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "抄表时间",fmt="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date meterAt;
     /**
      * 备注
      */
+    @ExcelAnnotation(value = "备注")
     private String remark;
 
     @TableField(fill = FieldFill.INSERT)
+    @ExcelAnnotation(value = "录入时间",fmt="yyyy-MM-dd HH:mm:ss")
     private Date createdAt;
 
     private Long createdBy;
-
+    @ExcelAnnotation(value = "录入人")
     private String createdName;
 
-    //@TableField(fill = FieldFill.INSERT_UPDATE)
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    @ExcelAnnotation(value = "修改时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @ExcelAnnotation(value = "修改时间",fmt="yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date modifiedAt;
 
@@ -105,5 +120,4 @@ public class FMeterRecord implements Serializable {
     private String modifiedName;
 
     private Integer isDelete;
-
 }
