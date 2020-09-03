875c0156fecb830917eb2cef2e0b3539e823c2e4
mzhongcai <javamzc@126.com>
2020-09-03 13:49:12 +0800

    增加获取数量的方法

--- estate-bill/src/main/java/com/estate/sdzy/system/service/SRoleService.java
+++ estate-bill/src/main/java/com/estate/sdzy/system/service/SRoleService.java
@@ -1,6 +1,5 @@
 package com.estate.sdzy.system.service;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.estate.common.exception.BillException;
 import com.estate.sdzy.system.entity.SMenu;
@@ -23,7 +22,7 @@ public interface SRoleService extends IService<SRole> {
     boolean save(SRole role,String token) throws BillException;
     boolean remove(Long id,String token) throws BillException;
     List<SRole> listRole(Map<String,String> map, Integer pageNo, Integer size, String token);
-    Page<SRole> listRoleNum(Map<String,String> map, Integer pageNo, Integer size, String token);
+    Integer listRoleNum(Map<String,String> map, Integer pageNo, Integer size, String token);
     String checkRoleMenuUser(Long id) throws BillException;
     boolean setRoleMenu(String roleId,String menuId, String token);
     List<SMenu> listRoleMenu(String token);
