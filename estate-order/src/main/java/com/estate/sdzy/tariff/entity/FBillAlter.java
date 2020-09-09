5ae4fa240abd90b31ff5cd5be32195acd809d14a
mzhongcai <javamzc@126.com>
2020-09-09 14:33:45 +0800

    增加属性及配置注解

--- estate-order/src/main/java/com/estate/sdzy/tariff/entity/FBillAlter.java
+++ estate-order/src/main/java/com/estate/sdzy/tariff/entity/FBillAlter.java
@@ -1,13 +1,18 @@
 package com.estate.sdzy.tariff.entity;
 
-import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
-import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
+import com.estate.sdzy.common.annotation.ExcelAnnotation;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * <p>
@@ -27,50 +32,15 @@ public class FBillAlter implements Serializable {
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
-    /**
-     * 调整时间，发起时间
-     */
-    private Date alterTime;
+    private Long compId;
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "物业公司")
+    private String compName;
 
-    /**
-     * 调整金额
-     */
-    private BigDecimal afterPrice;
-
-    /**
-     * 调整人
-     */
-    private String alterOwner;
-
-    /**
-     * 调整理由
-     */
-    private String alterReason;
-
-    /**
-     * 发起人
-     */
-    private String initiator;
-
-    /**
-     * 审核人
-     */
-    private String auditor;
-
-    /**
-     * 审核理由
-     */
-    private String audiReason;
-
-    /**
-     * 审核时间
-     */
-    private Date audiTime;
-
-    /**
-     * 审核是否通过
-     */
-    private String status;
+    private Long commId;
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "社区")
+    private String commName;
 
     /**
      * 账单id
@@ -78,9 +48,110 @@ public class FBillAlter implements Serializable {
     private Long billId;
 
     /**
-     * 上调还是下降
+     * 物业类型：房产、停车位
      */
-    private String alterType;
+    @ExcelAnnotation(value = "物业类型")
+    @TableField(exist = false)
+    private String propertyType;
+
+    /**
+     * 物业id
+     */
+    @TableField(exist = false)
+    private Long property_id;
+
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "物业编号")
+    private String propertyName;
+
+    /**
+     * 费用标准
+     */
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "费用标准")
+    private String costRuleName;
+
+    /**
+     * 账期
+     */
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "账期")
+    private String accountPeriod;
+
+    /**
+     * 费用金额
+     */
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "费用金额")
+    private BigDecimal price;
+
+    /**
+     * 账单状态
+     */
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "账单状态")
+    private String billState;
+
+    /**
+     * 状态
+     */
+    @ExcelAnnotation(value = "调整状态")
+    private String state;
+    /**
+     * 调整金额
+     */
+    @ExcelAnnotation(value = "调整金额")
+    private BigDecimal alterFee;
+
+    /**
+     * 调整原因
+     */
+    @ExcelAnnotation(value = "调整原因")
+    private String alterReason;
+
+    /**
+     * 调整人id
+     */
+    private Long alterBy;
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "调整人")
+    private String alterByName;
+
+    /**
+     * 调整时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @ExcelAnnotation(value = "调整时间")
+    private Date alterTime;
+
+    /**
+     * 审核意见
+     */
+    @ExcelAnnotation(value = "审核意见")
+    private String audiReason;
+    /**
+     * 审核人
+     */
+    private Long auditor;
+    @TableField(exist = false)
+    @ExcelAnnotation(value = "审核人")
+    private String auditorName;
+    @TableField(exist = false)
+    private String userName;//审核人用户名
+
+    /**
+     * 审核时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @ExcelAnnotation(value = "审核时间")
+    private Date auditTime;
 
 
+
+    /**
+     * 备注
+     */
+    @ExcelAnnotation(value = "备注")
+    private String remark;
 }
