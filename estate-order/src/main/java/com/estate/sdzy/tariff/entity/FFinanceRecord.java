873f60c30181165ad206750a40f67b6a84e02c2d
mzhongcai <javamzc@126.com>
2020-09-14 16:50:08 +0800

    增加账户名称、账户编号、业主名属性

--- estate-order/src/main/java/com/estate/sdzy/tariff/entity/FFinanceRecord.java
+++ estate-order/src/main/java/com/estate/sdzy/tariff/entity/FFinanceRecord.java
@@ -1,16 +1,18 @@
 package com.estate.sdzy.tariff.entity;
 
-import java.math.BigDecimal;
-import java.util.Date;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
-
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * <p>
@@ -41,6 +43,15 @@ public class FFinanceRecord implements Serializable {
     private Long accountId;
 
     /**
+     * 账户名称、账户编号、业主名
+     */
+    @TableField(exist = false)
+    private String accountName;
+    @TableField(exist = false)
+    private String accountNo;
+    @TableField(exist = false)
+    private String ownerName;
+    /**
      * 操作类型（支付，预存，取现）
      */
     private String operType;
@@ -63,6 +74,8 @@ public class FFinanceRecord implements Serializable {
     private String remark;
 
     @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createdAt;
 
     private Long createdBy;
