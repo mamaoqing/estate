c28d47bdd2d8f612de95bedf212d6cf7d9c4c9f7
mzhongcai <javamzc@126.com>
2020-09-14 09:10:53 +0800

    账户 服务类

---
+++ estate-order/src/main/java/com/estate/sdzy/tariff/service/FAccountService.java
@@ -0,0 +1,26 @@
+package com.estate.sdzy.tariff.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.estate.sdzy.tariff.entity.FAccount;
+
+import java.util.Map;
+
+/**
+ * <p>
+ * 账户 服务类
+ * </p>
+ *
+ * @author mzc
+ * @since 2020-09-10
+ */
+public interface FAccountService extends IService<FAccount> {
+
+    Page<FAccount> listAccount(Map<String,String> map,Integer pageNo, Integer size, String token);
+
+    boolean save(FAccount item,String token);
+
+    boolean saveOrUpdate(FAccount item,String token);
+
+    FAccount getAccount(Long ownerId,Long ruleId);
+}
