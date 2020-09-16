5b68e46442ca65a9313224a2ddc6c3cca3d4386f
mamaoqing <luyuna1121@163.com>
2020-09-16 16:02:36 +0800

    修改账单价格计算方式

--- estate-account/src/main/java/com/estate/sdzy/DoDebit.java
+++ estate-account/src/main/java/com/estate/sdzy/DoDebit.java
@@ -8,9 +8,7 @@ import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author mq
@@ -23,101 +21,114 @@ import java.util.UUID;
 public class DoDebit {
 
     public static void main(String[] args) {
-        Connection connection = null;
+
         Date now = new Date();
         Calendar c = Calendar.getInstance();
         c.setTime(now);
         c.add(Calendar.DATE, -1);
         Date time = c.getTime();
-        System.out.println(time);
-        String sql = "select aa.id account_id,aa.owner_id ,fee,cc.* from f_account aa,f_account_cost_item bb,f_bill cc where aa.id = bb.account_id and bb.property_id = cc.property_id and bb.property_type = cc.property_type and bb.rule_id = cc.cost_rule_id and cc.bill_time > ?";
-        Object[] objects = {time};
-        ResultSet resultSet = null;
+        String sql = "select id,owner_id fee from f_account aa where aa.fee > 0";
+
         try {
-            resultSet = ConnectUtil.executeQuery(sql, objects);
+            ResultSet resultSet = TransactionConnUtil.executeQuery(sql);
             while (resultSet.next()) {
-                long owner_id = resultSet.getLong("owner_id");
-                long account_id = resultSet.getLong("account_id");
-                // billId
-                long id = resultSet.getLong("id");
-                long compId = resultSet.getLong("comp_id");
-                long commId = resultSet.getLong("comm_id");
-                String billNo = resultSet.getString("bill_no");
-                String sqls = "select fee from f_account where id = "+account_id;
-                ResultSet set = ConnectUtil.executeQuery(sqls);
-                BigDecimal fee = new BigDecimal(0);
-                while (set.next()){
-                    fee = set.getBigDecimal(1);
-                }
-                System.out.println(fee+"<=--==-=-=-=-=>");
-                BigDecimal price = resultSet.getBigDecimal("price");
-                BigDecimal sale_price = resultSet.getBigDecimal("sale_price");
+                int account_id = resultSet.getInt("id");
+                int owner_id = resultSet.getInt("owner_id");
+                BigDecimal fee = resultSet.getBigDecimal("fee");
+                String billSql = "select bb.*,price-sale_price-pay_price+overdue_cost aaa from f_account_cost_item aa, f_bill bb where aa.property_type = bb.property_type and aa.property_id = bb.property_id and aa.account_id =? and aa.rule_id  = bb.cost_rule_id  and bb.state ='未支付' and is_payment='否'";
+                Object[] obj = {account_id};
+                ResultSet result = TransactionConnUtil.executeQuery(billSql, obj);
+                exeute(result, fee, account_id, now, owner_id);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void exeute(ResultSet result, BigDecimal fee, int account_id, Date now, int owner_id) {
+        Connection connection = null;
+        List<Object[]> list = new ArrayList<>();
+        BigDecimal count = new BigDecimal(0);
+        Object[] objs = null;
+        long id,compId = 0,commId = 0L;
+        String no = UUID.randomUUID().toString().replace("-", "");
+        BigDecimal price = new BigDecimal(0);
+        String payMent = "否";
+        String operType = "支付";
+        String payment_method = "自动扣费";
+        try {
+            connection = TransactionConnUtil.getConnection();
+            while (result.next()) {
+                // 账单id
+                id = result.getLong("id");
+                // 公司id
+                compId = result.getLong("comp_id");
+                // 社区id
+                commId = result.getLong("comm_id");
                 // 扣款省下的余额
                 BigDecimal money = new BigDecimal(0);
                 // 付的钱
                 BigDecimal payPrice = new BigDecimal(0);
                 // 是否支付
-                String payMent = "否";
-                String operType = "支付";
-                String state = "未支付";
-                String payment_method = "自动扣费";
-                String no = UUID.randomUUID().toString().replace("-", "");
-                // 保证总金额和余额不为空
+               payMent = "否";
+                // 支付状态
+                operType = "支付";
+                // 扣费方式
+                payment_method = "自动扣费";
+
+                // 结算的钱，减去优惠加上违约金减去已经支付的钱。
+                price = result.getBigDecimal("aaa");
                 if (null == price) {
                     price = new BigDecimal(0);
                 }
-                if (null == sale_price) {
-                    sale_price = new BigDecimal(0);
-                }
-                price = price.subtract(sale_price);
                 if (null == fee) {
                     fee = new BigDecimal(0);
                 }
+                count.add(price);
                 // 账户余额不小于账单总金额
-                if (!(fee.compareTo(price) < 1)) {
-                    money = fee.subtract(price);
-                    payPrice = price;
-                    payMent = "是";
-                    state = "已支付";
-                } else {
-                    payPrice = fee;
+                if ((fee.compareTo(count) < 1)) {
+                    break;
                 }
-                connection = TransactionConnUtil.getConnection();
-                // 添加f_finance_record数据
-                String insertFinanceFRecordSql = "insert into f_finance_record(comp_id,comm_id,no,account_id,oper_type,cost,owner_id,payment_method,created_at) " +
-                        "values(?,?,?,?,?,?,?,?,?)";
-                Object[] objs = {compId, commId, no, account_id, operType, payPrice, owner_id, payment_method,now };
-                Integer integer = TransactionConnUtil.executeUpdate(insertFinanceFRecordSql, objs, true);
-
-                // 添加f_finance_bill_record数据
-                String insertFinanceBillRecord = "insert into f_finance_bill_record(comp_id,comm_id,finance_record_id,bill_id,cost,created_at)" +
-                                                                            "values(?,?,?,?,?,?)";
-                Object[] o = {compId, commId,integer,id,payPrice,now};
-                TransactionConnUtil.executeUpdate(insertFinanceBillRecord,o);
-                // 更新f_account
-                String updateAccount = "update f_account set fee = ? where id = ?";
-                Object[] accout = {money,account_id};
-                Integer integer2 = TransactionConnUtil.executeUpdate(updateAccount, accout);
-                System.out.println(integer2+"||--------------------||"+payPrice);
-
+                Object[] o = {compId, commId,id,payPrice,now,null};
                 // 更新f_bill
-                String updateBill = "update f_bill set pay_price = ? , is_payment = ? ,state = ? where id = ?";
-                Object[] b = {payPrice,payMent,state,id};
-                Integer integer1 = TransactionConnUtil.executeUpdate(updateBill, b);
-
-                // 提交事务。
-                connection.commit();
+                String updateBill = "update f_bill set pay_price = ? , is_payment = ? where id = ?";
+                Object[] b = {payPrice,payMent,id};
+                TransactionConnUtil.executeUpdate(updateBill, b);
+                objs = new Object[]{compId, commId, no, account_id, operType, payPrice, owner_id, payment_method, now};
+                list.add(o);
             }
-        } catch (SQLException sqlException) {
+            objs = new Object[]{compId, commId, no, account_id, operType, count, owner_id, payment_method, now};
+
+            // 添加f_finance_record数据
+            String insertFinanceFRecordSql = "insert into f_finance_record(comp_id,comm_id,no,account_id,oper_type,cost,owner_id,payment_method,created_at) " +
+                    "values(?,?,?,?,?,?,?,?,?)";
+
+            Integer integer = TransactionConnUtil.executeUpdate(insertFinanceFRecordSql, objs, true);
+            String updateAccount = "update f_account set fee = ? where id = ?";
+            Object[] accout = {fee.subtract(count),account_id};
+            
+            Integer integer2 = TransactionConnUtil.executeUpdate(updateAccount, accout);
+            for (Object[] objects : list) {
+                String insertFinanceBillRecord = "insert into f_finance_bill_record(comp_id,comm_id,bill_id,cost,created_at,finance_record_id)" +
+                        "values(?,?,?,?,?,?)";
+                objects[5] = integer;
+                TransactionConnUtil.executeUpdate(insertFinanceBillRecord,objects);
+            }
+            connection.commit();
+        } catch (SQLException | ClassNotFoundException sqlException) {
             try {
                 connection.rollback();
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
             sqlException.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         }
+
+
 
     }
 
