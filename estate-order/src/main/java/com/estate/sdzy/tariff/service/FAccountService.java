93d2e52c62ecd54cb7d3f4003e3887202432eac2
mzhongcai <javamzc@126.com>
2020-09-18 09:03:56 +0800

    添加根据账户id获取账户消费项的方法

--- estate-order/src/main/java/com/estate/sdzy/tariff/service/FAccountService.java
+++ estate-order/src/main/java/com/estate/sdzy/tariff/service/FAccountService.java
@@ -3,6 +3,7 @@ package com.estate.sdzy.tariff.service;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.estate.sdzy.tariff.entity.FAccount;
+import com.estate.sdzy.tariff.entity.FAccountCostItem;
 
 import java.util.List;
 import java.util.Map;
@@ -19,7 +20,7 @@ public interface FAccountService extends IService<FAccount> {
 
     Page<FAccount> listAccount(Map<String,String> map,Integer pageNo, Integer size, String token);
 
-    boolean save(FAccount item,String token);
+    boolean saveAccount(FAccount item,String token);
 
     boolean saveOrUpdate(FAccount item,String token);
 
@@ -28,4 +29,8 @@ public interface FAccountService extends IService<FAccount> {
     List<FAccount> getAccountByOwnerId(Long ownerId, String token);
 
     List<Map<String,String>> listTypes(Map<String,String> map,String token);
+
+    boolean doUpdate(FAccount account, String token);
+
+    List<FAccountCostItem> getAccountItemByAccountId(String accountId);
 }
