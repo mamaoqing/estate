d86423cb97cad4e0c7d2dc5911200036cc4d7432
geqx <1255940707@qq.com>
2020-09-24 13:44:02 +0800

    修改计算公式

--- estate-wechat/src/main/java/com/estate/sdzy/wechat/controller/PayController.java
+++ estate-wechat/src/main/java/com/estate/sdzy/wechat/controller/PayController.java
@@ -52,27 +52,42 @@ public class PayController {
         String commId = request.getParameter("commId");
         String compId = request.getParameter("compId");
         String payPrice = request.getParameter("payPrice");
-
+        BigDecimal pay = new BigDecimal(0.01);
         String payment_method = "微信支付";
         Date created_at = new Date();
         Integer ownerId = null;
+        BigDecimal aaa = new BigDecimal(0);
         String ownerSql = "select id from r_owner where wx_openid = ? and comp_id = ?";
         Connection connection = null;
         log.info("接收到的参数有 \tlistBillId:{},commId:{},compId:{},payPrice:{},openid:{}", listBillId, commId, compId, payPrice, openid);
+        String billSql = "select bb.*,price+sale_price-pay_price+overdue_cost aaa from   f_bill bb where   bb.id in (" + listBillId + ")   ";
         String insertf_finance_record_draftSql = "insert into f_finance_record_draft (comp_id,comm_id,no,oper_type,cost,owner_id,payment_method,created_at) values(?,?,?,?,?,?,?,?)";
-        String insertf_finance_bill_record_draftSql = "insert into f_finance_bill_record_draft (comp_id,comm_id,finance_record_id,bill_id,cost,created_at) select comp_id,comm_id,?,id,(price+sale_price+overdue_cost-pay_price),now() from f_bill where id in (" + listBillId + ") ";
+        String insertf_finance_bill_record_draftSql = "insert into f_finance_bill_record_draft (comp_id,comm_id,finance_record_id,bill_id,cost,created_at) (?,?,?,?,?,now())";
         try {
             ResultSet resultSet = ConnectUtil.executeQuery(ownerSql, new Object[]{openid, compId});
             if (resultSet.next()) {
                 ownerId = resultSet.getInt("id");
             }
             Object[] objects = {compId, commId, no, oper_type, payPrice, ownerId, payment_method, created_at};
-
+            ResultSet result = TransactionConnUtil.executeQuery(billSql );
             connection = TransactionConnUtil.getConnection();
             Integer integer = TransactionConnUtil.executeUpdate(insertf_finance_record_draftSql, objects, true);
             log.info("执行的sql添加语句:{},传入的参数是：{}", insertf_finance_bill_record_draftSql, integer);
-            TransactionConnUtil.executeUpdate(insertf_finance_bill_record_draftSql, new Object[]{integer});
+            boolean b =true;
+            while(result.next()&&b) {
+                aaa = result.getBigDecimal("aaa");
+                if(pay.compareTo(aaa)==1){
+                    aaa=aaa.subtract(pay);
+                    b =true;
+                }else {
+                    pay=aaa;
+                    aaa=new BigDecimal(0);
+                    b=false;
+                }
+                Object[] objectb = {compId, commId, integer, result.getBigDecimal("id"), pay};
+                TransactionConnUtil.executeUpdate(insertf_finance_bill_record_draftSql, objectb);
 
+            }
             PayResponse payResponse = payService.create(Double.valueOf(payPrice), openid, integer + "", "物业收款");
             request.setAttribute("res", payResponse);
             log.info("微信支付结果：{}", payResponse);
