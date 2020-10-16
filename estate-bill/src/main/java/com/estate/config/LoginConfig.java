c1c0dfc34310bb4edc5c78ebdbba8b45d2280c34
mzhongcai <javamzc@126.com>
2020-10-16 15:13:53 +0800

    图片转存

--- estate-bill/src/main/java/com/estate/config/LoginConfig.java
+++ estate-bill/src/main/java/com/estate/config/LoginConfig.java
@@ -1,15 +1,8 @@
 package com.estate.config;
 
-import com.estate.interceptor.BuyInterceptor;
 import com.estate.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisClusterConnection;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisSentinelConnection;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
---
+++ estate-bill/src/main/java/com/estate/sdzy/system/controller/UeditorController.java
@@ -0,0 +1,29 @@
+package com.estate.sdzy.system.controller;
+
+import com.estate.common.controller.BaseController;
+import com.estate.util.UploadFileToFileServer;
+import org.json.JSONException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/sdzy/ueditor")
+public class UeditorController extends BaseController {
+
+        @RequestMapping(value = "/exec")
+        @ResponseBody
+        public String exec(HttpServletRequest request,@RequestParam("image") MultipartFile file) throws UnsupportedEncodingException, JSONException {
+            String path = null;
+            UploadFileToFileServer server = new UploadFileToFileServer();
+            try {
+                path = server.fileUpload(file.getBytes(),file.getOriginalFilename());
+            }catch (Exception e){
+
+            }
+            return path;
+        }
+}
